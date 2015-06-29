d62b12f4981ddb1a57db89e663cc19f43674fff5
Roberto Soares <roberto.tech@gmail.com>
2015-06-29 20:54:56 -0300

    showing about section #ugly #23 @recarreira

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @about = About.first
     @organizers = Organizer.all
     @staff_members = StaffMember.all
     @sponsors = Sponsor.all
