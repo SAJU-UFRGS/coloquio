1352af80df4addd216f4ae6ed88fa0681ac84a15
anikarni <adrianolic@gmail.com>
2015-06-30 09:28:24 -0300

    #6 - @anikarni - Adds images and other media types to posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,12 @@
 module PostsHelper
   def media_content post
     case post.media_type
-    when 'video/mp4'
+    when 'video/mp4', 'video/webm', 'video/ogg'
       render 'media/video', {post: post}
-    when 'audio/mpeg'
+    when 'audio/mpeg', 'audio/ogg', 'audio/wav'
       render 'media/audio', {post: post}
+    when 'image'
+      render 'media/image', {post: post}
     end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
+
+  def media_type_enum
+    [['video/mp4'], ['video/ogg'], ['video/webm'], ['audio/wav'],
+     ['audio/mpeg'], ['audio/ogg'], ['image']]
+  end
 end
