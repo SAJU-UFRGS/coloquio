9e4eb018dc72a5a1bf1c0874276840877057cb8c
Roberto Soares <roberto.tech@gmail.com>
2015-07-22 20:53:35 -0300

    monkey patch to strip tags from edited values with CKEditor #23 #22

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,3 +1,21 @@
+require 'action_view/helpers/sanitize_helper'
+
 Ckeditor.setup do |config|
   config.assets_languages = ['pt']
+end
+
+module RailsAdmin
+  module Config
+    module Fields
+      module Types
+        class CKEditor < RailsAdmin::Config::Fields::Types::Text
+          include ActionView::Helpers::SanitizeHelper
+
+          register_instance_option :pretty_value do
+            strip_tags value
+          end
+        end
+      end
+    end
+  end
 end
