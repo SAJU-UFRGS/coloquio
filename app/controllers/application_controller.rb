02897814f271b67cd1a058b4a9f9a5b95838545b
Roberto Soares <roberto.tech@gmail.com>
2015-07-29 20:48:16 -0300

    handling empty about

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_page_title
+
+  private
+
+  def set_page_title
+    @about = About.first || About.new
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @about = About.first
     @organizers = Organizer.all
     @staff_members = StaffMember.all
     @sponsors = Sponsor.all
