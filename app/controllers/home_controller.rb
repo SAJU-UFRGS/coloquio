66b98e6f1b696a57e1fd4bf25abe1f5500e84998
anikarni <anikarni@gmail.com>
2015-08-19 21:05:00 -0300

    #8 - @anikarni - Fixes organizers css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @organizers = Organizer.all
+    @organizers_with_image = Organizer.select { |organizer| organizer.has_image? }
+    @organizers_without_image = Organizer.select { |organizer| !organizer.has_image? }
     @staff_members = StaffMember.all
     @sponsors = Sponsor.all
     @supporters = Supporter.all
--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,3 +1,7 @@
 class Organizer < ActiveRecord::Base
   validates :name, presence: true
+
+  def has_image?
+    !self.image_url.empty?
+  end
 end
