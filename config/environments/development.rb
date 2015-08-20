46abfd95cd8928e003e417a1e57cc1b0ad3eaa99
Mukiza Andrew <andrewmukiza@gmail.com>
2015-08-19 21:04:11 -0300

    reads mailler auth from environment variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Mailer smpt configuration.
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               ENV['DOMAIN'],
+  #   user_name:            ENV['EMAIL'],
+  #   password:             ENV['PASSWORD'],
+  #   authentication:       :plain,
+  #   enable_starttls_auto: true
+  # }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
-    user_name:            '<email@gmail.com>',
-    password:             '<password>',
+    domain:               ENV['DOMAIN'],
+    user_name:            ENV['EMAIL'],
+    password:             ENV['PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
   }
