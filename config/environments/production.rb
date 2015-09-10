d28eff29d0f5c3cd82ce534048e093cca400d943
Mukiza Andrew <andrewmukiza@gmail.com>
2015-09-09 21:03:12 -0300

    #30 reset the address back to smpt.gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['EMAIL'],
+    address:              'smtp.gmail.com',
     port:                 587,
     domain:               ENV['DOMAIN'],
     user_name:            ENV['EMAIL'],
