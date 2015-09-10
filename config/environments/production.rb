b37d360b7ceba1f2140ae3db1ac507c882659584
Mukiza Andrew <andrewmukiza@gmail.com>
2015-09-09 21:21:08 -0300

    #30 adds default host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'coloquiovivo.herokuapp.com' }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
